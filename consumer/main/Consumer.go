c197d2b8c60f141f51f22f5dc734ea70f6695fae
cbod <bodenbender@unknown404.de>
2018-06-15 15:40:00 +0200

    added plan of action in comments

--- consumer/main/Consumer.go
+++ consumer/main/Consumer.go
@@ -31,3 +31,10 @@ func receiveShow(w http.ResponseWriter, r *http.Request) {
 	data := vars["data"]
 	fmt.Fprintln(w, "This is how I can accept data:", data)
 }
+
+// take a look at https://github.com/ET-CS/golang-response-examples/blob/master/ajax-json.go
+// plan of action: build template html with content div.
+// poll for update via ajax call to rest api
+// todo evaluate if all content or only new content should be sent
+// if all content : delete content of div, put in result of ajax call
+// if new content : append new content to div, check for duplicates?
