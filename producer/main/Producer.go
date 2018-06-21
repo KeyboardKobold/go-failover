864eab9e47009f790ee7a215047ec9a98edfb887
cdbd25 <ChrisBodenbender@gmail.com>
2018-06-21 20:38:44 +0200

    Added compose file for complete project + changes for kubernestes compatability

--- producer/main/Producer.go
+++ producer/main/Producer.go
@@ -8,23 +8,33 @@ import (
 	"time"
 	"net/http"
 	"strings"
+	"os"
+	"errors"
 )
 
 func main() {
 	count := 1
 	ticker := time.NewTicker(1000 * time.Millisecond)
 	for range ticker.C {
-		postEvent(count)
-		count = count + 1
+		err := postEvent(count)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			count = count + 1
+		}
 	}
 }
 
-func postEvent(count int){
-	requestUrl := "http://localhost:8080/addEvent"
+func postEvent(count int) error {
+	requestHost := os.Getenv("CONSUMER_HOST")
+	if requestHost == "" {
+		panic(errors.New("CONSUMER_HOST was not set"))
+	}
+	requestUrl := "http://" + requestHost + "/addEvent"
 	form := url.Values{
 		"eventId": {strconv.Itoa(count)},
 	}
-	fmt.Printf(strconv.Itoa(count)+ "\n")
+	fmt.Printf(strconv.Itoa(count) + "\n")
 	fmt.Printf(form.Get("eventId") + "\n")
 
 	req, err := http.NewRequest("POST", requestUrl, strings.NewReader(form.Encode()))
@@ -32,7 +42,8 @@ func postEvent(count int){
 	client := http.Client{}
 	rsp, err := client.Do(req)
 	if err != nil {
-		panic(err)
+		//panic(err)
+		return err
 	}
 
 	defer rsp.Body.Close()
@@ -41,4 +52,5 @@ func postEvent(count int){
 		panic(err)
 	}
 	fmt.Println(string(bodyByte))
+	return nil
 }
